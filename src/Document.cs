a83bdd1c87de51f72b303ac8eb1093cfbbb837e6
Nom <hittl.r@gmail.com>
2023-01-21 12:36:42 +0100

    Add default values into document

--- src/Document.cs
+++ src/Document.cs
@@ -4,7 +4,7 @@ namespace Moravia.Homework
 {
     public class Document
     {
-        public string Title { get; set; }
-        public string Text { get; set; }
+        public string Title { get; set; } = "";
+        public string Text { get; set; } = "";
     }
 }
